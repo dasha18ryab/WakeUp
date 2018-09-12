fe84081877353c8e9e8abf80b5ccd48d0701ae6b
dasha18ryab <39916547+dasha18ryab@users.noreply.github.com>
2018-09-12 10:45:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function change1() {
+	
+	document.getElementById('pic1').src="heart.gif";
+}
+function change2() {
+	
+	document.getElementById('pic1').src="bubna.png";
+}
+
+function change3() {
+	
+	document.getElementById('pic1').src="trefa.png";
+}
+
+function change4() {
+	
+	document.getElementById('pic1').src="pika.jpg";
+}
