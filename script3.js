d1b71a733b9e7387c87d648b1de1b74aa05dd489
dasha18ryab <39916547+dasha18ryab@users.noreply.github.com>
2018-09-26 10:56:48 +0300

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,22 @@
+function middle() {
+
+var N = parseInt(document.getElementById('N').value);
+
+var sim1=0,sim2=1;
+
+for (var i=0;i<N;i++){
+
+	res= sim1 + sim2;
+	
+	alert(res)
+	console.log(res);
+	document.getElementById('result').innerHTML = res;
+
+	
+	sim1 = sim2 ;
+
+	sim2=res;
+}
+
+}
+	
