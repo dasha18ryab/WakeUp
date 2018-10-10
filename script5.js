fcd54cefba9a886d7830f47aa623fc090131680d
dasha18ryab <39916547+dasha18ryab@users.noreply.github.com>
2018-10-10 12:01:35 +0300

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,31 @@
+﻿function middle()
+
+{
+var A = parseInt(document.getElementById('A').value),
+
+
+B = parseInt(document.getElementById('B').value),
+ i, j, Nums = [];
+
+			
+		
+for (i = A; i <= B; i++)
+ 
+		
+{
+			
+
+for (j = 0; j < i; j++) Nums.push(i);
+		
+
+}
+		
+
+console.log(Nums);
+		
+		
+
+document.getElementById('result').innerHTML = Nums.join(", ");
+
+}
+
