8fa49f24070cf53008d3368df5b543a25e27c611
dasha18ryab <39916547+dasha18ryab@users.noreply.github.com>
2018-10-10 11:48:32 +0300

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,19 @@
+﻿function middle() {
+
+var N = parseInt(document.getElementById('N').value);
+var K = parseInt(document.getElementById('K').value);
+
+var summ=0;
+for (var i=1;i<=N;i++){
+
+summ= summ + Math.pow(i,K);
+
+
+//console.log(summ);
+document.getElementById('result').innerHTML = summ;
+
+
+
+}
+
+}
---
+++ script6.js
@@ -0,0 +1,22 @@
+﻿function middle(){
+	
+		
+
+var A = parseInt(document.getElementById('A').value),
+ K = 0, Summ = 0;
+			
+
+
+while (Summ < A) Summ += (1/ ++K);
+		
+		
+
+document.getElementById('K').innerHTML = K;
+		
+
+document.getElementById('Summ').innerHTML = Summ;
+
+	
+	
+}
+
