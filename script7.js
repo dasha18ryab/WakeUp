3231403a124cedfddc36eec4ae48df19536abda1
dasha18ryab <39916547+dasha18ryab@users.noreply.github.com>
2018-10-31 11:39:52 +0200

    Add files via upload

---
+++ script7.js
@@ -0,0 +1,44 @@
+﻿var d, m,y;
+ function Fdate() {
+ 	 d=document.getElementById('Text').value;
+
+
+ 	
+ 	return d;
+ 	
+
+}
+ function Fmonth() {
+ 	 m=(document.getElementById('Text').value);
+ 	
+ 	return m;
+
+}
+ function Fyear() {
+ 	 y=document.getElementById('Text').value;
+ 	
+ 	return y;
+
+}
+
+
+
+
+		function formatDate() {
+
+			
+     var Day_names = ["воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота"];
+  var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+let date = new Date( y,m,d )
+  var day = date.getDay();
+  var monthIndex = date.getMonth();
+  var year =date.getFullYear();
+ var nextYear= date.getDate()+' '+Day_names[date.getDay(date.setYear(date.getFullYear() + 1))] + ' ' + monthNames[monthIndex] + ' ' + (year+1);
+ var thisYear=date.getDate()+' '+Day_names[day]+ ' ' + monthNames[monthIndex] + ' ' + year;
+  return thisYear + '/' + nextYear;
+}
